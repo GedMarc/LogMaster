c26fdcd9da479b25c2b267bf31692fc6fed21b37
GedMarc <ged_marc@hotmail.com>
2019-04-29 07:55:22 +0200

    Update for memory leak and security modules separation

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,6 +1,7 @@
 module com.jwebmp.logmaster {
 	exports com.jwebmp.logger;
 	exports com.jwebmp.logger.logging;
+	exports com.jwebmp.logger.handlers;
 
 	requires transitive java.logging;
 	requires java.validation;
